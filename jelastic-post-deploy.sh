09010058f9c116285def9d4cc783bc136181d78a
guillaume hochet <guillaume.hochet@gmail.com>
2022-02-14 16:15:41 +0100

    fix script

--- jelastic-post-deploy.sh
+++ jelastic-post-deploy.sh
@@ -1,10 +1,10 @@
 # Install dependencies
-composer install
+cd /var/www/webroot/ROOT && composer install
 
 # Setup ssh keys
-mkdir /var/www/webroot/ROOT/config/jwt
-openssl genpkey -out /var/www/webroot/ROOT/config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
-openssl pkey -in config/jwt/private.pem -out /var/www/webroot/ROOT/config/jwt/public.pem -pubout
+# mkdir /var/www/webroot/ROOT/config/jwt
+# openssl genpkey -out  /var/www/webroot/ROOT/config/jwt/private.pem -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
+# openssl pkey -in config/jwt/private.pem -out /var/www/webroot/ROOT/config/jwt/public.pem -pubout
 
 # Clear ande setup
 php /var/www/webroot/ROOT/bin/console cache:clear --env=prod
