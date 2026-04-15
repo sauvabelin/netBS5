db10b0f859aad58529f7d7d78458657a15ec5c93
Iacopo Sprenger <iacopo.sprenger@proton.me>
2026-04-15 11:40:35 +0200

    fix: fixed navigation

--- assets/lib/modal_dom.js
+++ assets/lib/modal_dom.js
@@ -12,6 +12,13 @@ export function createModal(content) {
     const bsModal = new bootstrap.Modal(el);
     bsModal.show();
 
+    // Initialize Select2 widgets inside the modal (if Select2Triggerer is available)
+    if (typeof Select2Triggerer !== 'undefined') {
+        const $el = $(el);
+        Select2Triggerer.triggerSelect2($el.find('[data-type="select2"]'));
+        Select2Triggerer.triggerAjaxSelect2($el.find('[data-type="ajax-select2"]'));
+    }
+
     el.addEventListener('hidden.bs.modal', () => {
         bsModal.dispose();
         el.remove();
