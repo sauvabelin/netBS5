5599d98bfe9d93789c194e00937d140c74ba5a13
Iacopo Sprenger <iacopo.sprenger@proton.me>
2026-05-19 07:28:49 +0200

    feat(identity): ErrorController + friendly access-denied page

---
+++ src/Identity/Controller/ErrorController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Identity\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ErrorController extends AbstractController
+{
+    #[Route('/oidc-error', name: 'oidc_error', methods: ['GET'])]
+    public function __invoke(Request $request): Response
+    {
+        return $this->render('identity/error.html.twig', [
+            'error' => $request->query->get('error', 'unknown'),
+            'errorDescription' => $request->query->get('error_description', ''),
+            'errorHint' => $request->query->get('error_hint', ''),
+        ], new Response('', Response::HTTP_FORBIDDEN));
+    }
+}
